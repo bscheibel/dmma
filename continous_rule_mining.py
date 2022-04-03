92ff141f5ae26351c5ec818cc2bdd6944e968e01
bscheibel <42147158+bscheibel@users.noreply.github.com>
2022-04-03 10:52:21 +0200

    Update continous_rule_mining.py

--- continous_rule_mining.py
+++ continous_rule_mining.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2022  Beate Scheibel
+# This file is part of edt-runtime.
+#
+# edt-runtime is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# edt-runtime is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# edt-runtime.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import pandas as pd
 import numpy as np
 np.seterr(divide='ignore')
