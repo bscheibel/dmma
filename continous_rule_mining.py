94bc17403a61dd326d45609bcde559ea1cb15518
bscheibel <42147158+bscheibel@users.noreply.github.com>
2022-06-27 07:32:40 +0200

    Update continous_rule_mining.py

--- continous_rule_mining.py
+++ continous_rule_mining.py
@@ -10,7 +10,7 @@
 # PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# edt-runtime.  If not, see <http://www.gnu.org/licenses/>.
+# dmma.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import pandas as pd
