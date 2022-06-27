40cc7327eaebf38f962aaff31eab381693e8f0ec
bscheibel <42147158+bscheibel@users.noreply.github.com>
2022-06-27 07:32:29 +0200

    Update continous_rule_mining.py

--- continous_rule_mining.py
+++ continous_rule_mining.py
@@ -1,11 +1,11 @@
 # Copyright (C) 2022  Beate Scheibel
-# This file is part of edt-runtime.
+# This file is part of dmma.
 #
-# edt-runtime is free software: you can redistribute it and/or modify it under the terms
+# dmma is free software: you can redistribute it and/or modify it under the terms
 # of the GNU General Public License as published by the Free Software Foundation,
 # either version 3 of the License, or (at your option) any later version.
 #
-# edt-runtime is distributed in the hope that it will be useful, but WITHOUT ANY
+# dmma is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 # PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
